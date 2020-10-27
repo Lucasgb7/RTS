80f063f1338cf816cfdd88949ae3888b569549d8
Lucas Cunha <lucasgb7_ks@hotmail.com>
2020-10-27 17:41:26 -0300

    inserindo leitura de teclas e variaveis de tempo

--- Threads/pthreads.c
+++ Threads/pthreads.c
@@ -7,7 +7,36 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
-#define LENGTH 1500 
+#include <termios.h>
+#include <fcntl.h>
+#define LENGTH 1500
+
+int kbhit(void)
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
 
 void *conveyorBelt_A(void *i){
     int *x = (int *)i;
@@ -34,9 +63,18 @@ void *conveyorBelt_C(void *i){
     return NULL;
 }
 
+
+ 
+
 int main() {
+    // Contagem nas esteiras
     int sum = 0;
     int a = 0, b = 0, c = 0;
+    int weight[LENGTH];
+    // Variaveis para calculo de tempo
+    clock_t start, end;
+    double cpu_time_used;
+    // double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     // Inicializa esteiras com 0 produtos
     printf("Contagem inicial\n"); 
     printf("Esteira A: %d\nEsteira B: %d\nEsteira C: %d\n", a, b, c);
@@ -57,6 +95,18 @@ int main() {
         return 1;
     }
 
+    printf("\n\n");
+    while (!kbhit())
+        printf("1 - Add na Esteira A\n");
+        printf("2 - Add na Esteira B\n");
+        printf("3 - Add na Esteira C\n");
+        printf("ESC - SAIR \n");
+        puts("Pressione uma tecla!");
+        if((int)(getchar()) == 27){
+            printf("");
+        }
+
+    
     sum = a + b + c;
 
     // Aguarda as outras threads terminarem
