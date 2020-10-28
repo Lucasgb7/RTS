f1006295a57856e4186b3118b1d6fc8e71bfe861
sovm <luizzimmermann@edu.univali.br>
2020-10-28 16:43:27 -0300

    Teste openMP

--- Threads/openMP.c
+++ Threads/openMP.c
@@ -14,6 +14,10 @@ gcc -o openMP -fopenmp openMP.c
 #define LENGTH 10       // Maximo de produtos
 #define MAX_WEIGHT 10   // Peso maximo dos produtos
 
+int total_items;
+int weights[LENGTH];
+int previous_items;
+
 int kbhit(void)
 {
 	struct termios oldt, newt;
@@ -41,11 +45,74 @@ int kbhit(void)
 	return 0;
 }
 
+int random_weight(){
+    int x = rand() % MAX_WEIGHT;
+    // Caso gerar um valor igual a zero, retorna 1
+    // printf("\tPeso+: %d", x);
+    if (x == 0)
+        return 1;
+    else return x;
+}
+
+int update_weights(int weights[])
+{
+    // Soma os pesos que estão armazenados no vetor
+    for (int i = 0; i < LENGTH; i++)
+    {
+        sum_weights += weights[i];
+    }
+    return sum_weights;
+}
+
+void update_display(int total_items, int sum_weights)
+{
+    printf("\n\n-----L C D-----");
+    printf("\nNº de Itens: %d", total_items);
+    printf("\nPeso Total: %d kg", sum_weights);
+}
+
+int input_simulation()
+{
+    int i = (rand() % 3) + 1; // Sorteia uma das 3 esteiras para inserir
+    printf("\nEsteira[%d]: ", i);
+    return i;
+}
+
 int main()
 {
-    #pragma omp parallel
+    total_items = 0;
+    previous_items = 0;
+
+    #pragma omp parallel num_threads(3)
     {
-        int id = omp_get_thread_num();
-        printf("Thread[%d]", id);
+
+        #pragma omp sections
+        {
+            // Primeira thread
+            #pragma section
+            {
+                while (total_items < LENGTH)
+                {
+                    if (kbhit())
+                    {
+                        if (getchar() == '1') // Caso digite: 1
+                        {
+                            #pragma omp critical     // Bloqueia a variavel
+                            {                      
+                                total_items += 100;                     // Atualiza o numero de itens
+                                weights[total_items] = random_weight(); // Adiciona o peso do item no vetor
+                            }                        // Libera a variavel
+                            printf("\nA++");
+                        }
+                    }
+                    // Atualiza display
+                    if (total_items != previous_items){
+                        update_display(total_items, update_weights(weights));
+                        previous_items = total_items;
+                    }
+                }
+            }
+            //-----------------------------//
+        }   
     }
 }
