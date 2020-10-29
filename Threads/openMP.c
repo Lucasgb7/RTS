682df8819457749eb8eab0da9e84ee2873d1d680
Lucas Cunha <lucasgb7_ks@hotmail.com>
2020-10-28 23:12:21 -0300

    capturando tempo

--- Threads/openMP.c
+++ Threads/openMP.c
@@ -11,40 +11,25 @@ gcc -o openMP -fopenmp openMP.c
 #include <fcntl.h>      //  KBHIT
 #include <omp.h>        //  OpenMP Library
 
-#define LENGTH 10       // Maximo de produtos
+#define LENGTH 1500       // Maximo de produtos
 #define MAX_WEIGHT 10   // Peso maximo dos produtos
 
 int total_items;
 int sum_weights;
 int weights[LENGTH];
 int previous_items;
+int input_array[LENGTH];
 
-int kbhit(void)
+int input_generator()
 {
-	struct termios oldt, newt;
-	int ch;
-	int oldf;
-	
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-	
-	ch = getchar();
-	
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	fcntl(STDIN_FILENO, F_SETFL, oldf);
-	
-	if(ch != EOF)
-	{
-		ungetc(ch, stdin);
-		return 1;
-	}
-	
-	return 0;
+    int i = rand() % 5; 
+    if (i == 0){
+        return 1;
+    }else{
+        return 0;
+    }
 }
+
 
 int random_weight(){
     int x = rand() % MAX_WEIGHT;
@@ -72,47 +57,50 @@ void update_display(int total_items, int sum_weights)
     printf("\nPeso Total: %d kg", sum_weights);
 }
 
-int input_simulation()
-{
-    int i = (rand() % 3) + 1; // Sorteia uma das 3 esteiras para inserir
-    printf("\nEsteira[%d]: ", i);
-    return i;
-}
-
 int main()
 {
+    // Variaveis para calculo de tempo
+    srand(time(NULL));
+    clock_t start, end;
+    double cpu_time_used;
+
+    weights[LENGTH] = 0;
     total_items = 0;
     previous_items = 0;
-    while (1)
+#pragma omp parallel shared(total_items, previous_items, weights) num_threads(3)
     {
-        #pragma omp parallel shared(total_items, previous_items, weights) num_threads(3)
+        printf("Thread %d iniciada.\n", omp_get_thread_num());
+        while (total_items < LENGTH)
         {
-                printf("Thread %d iniciada.\n", omp_get_thread_num());
-                while (total_items < LENGTH)
+            if (input_generator)
+            {
+#pragma omp critical // Bloqueia a variavel
                 {
-                    if (kbhit())
-                    {
-                        
-                        #pragma omp critical     // Bloqueia a variavel
-                        {                      
-                            total_items += 100;                     // Atualiza o numero de itens
-                            weights[total_items] = random_weight(); // Adiciona o peso do item no vetor
-                        }                        // Libera a variavel
-                        printf("Thread %d ativada.\n", omp_get_thread_num());
-                        
-                    }
-                    // Atualiza display
-                    if (total_items != previous_items){
-                        update_display(total_items, update_weights(weights));
-                        previous_items = total_items;
-                    }
+                    total_items += 1;                       // Atualiza o numero de itens
+                    weights[total_items] = random_weight(); // Adiciona o peso do item no vetor
+                }                                           // Libera a variavel
+                printf("\nThread %d ativada.\n", omp_get_thread_num());
+                // Atualiza display
+                if (total_items != previous_items)
+                {
+                    update_display(total_items, update_weights(weights));
+                    previous_items = total_items;
                 }
-            }    
-        }  
-
-        update_display(total_items, update_weights(weights));   // Printa o display
-
+            }
+        }
     }
-    
-    
+     // Calcula o tempo para pesagem
+    start = clock();
+    update_weights(weights);
+    end = clock();
+    double weight_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    // Calcula a taxa de atualização do display
+    start = clock();
+    update_display(total_items, update_weights(weights));
+    end = clock();
+    double display_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    // Fornece os tempos testados
+    printf("\nTaxa de atualização: %f segundos", display_time);
+    printf("\nTempo de contagem: %f segundos", weight_time*LENGTH);
+    printf("\nTempo de pesagem: %f segundos\n", weight_time);
 }
